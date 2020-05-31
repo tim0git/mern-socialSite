b554836787741a15dcc637f4e4321ae451d55995
tim0git <tim.doolan88@icloud.com>
2020-06-01 00:43:49 +0100

    connectDB now only has one env

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -3,11 +3,11 @@ const ENV = process.env.NODE_ENV || "development";
 const mongoose = require("mongoose");
 // require in mongoose to set up the db connection
 const config = require("config");
-// require in config to access the defaut.json URI file
+// require in config to access the default.json URI file
 const db = config.get("mongoURI");
 // get mongoURI from default.json
 
-const connectDB = {
+/*const connectDB = {
   test: (test = async () => {
     try {
       await mongoose.connect(`mongodb://127.0.0.1/test`, {
@@ -39,6 +39,23 @@ const connectDB = {
       process.exit(1);
     }
   }),
-};
+};*/
 
-module.exports = connectDB[ENV];
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB connected Development Cloud");
+  } catch (err) {
+    console.log(err.message);
+    // if error exit process with failure
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
